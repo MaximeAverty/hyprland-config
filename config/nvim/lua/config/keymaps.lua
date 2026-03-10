1b9698f293fb8e5c7d6b99bb4fb311ddae1f1753
Maxime <maxime.averty.pro@gmail.com>
2026-03-10 22:51:45 +0100

    add yazi as filemanager | new kitty theme | update keymap

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+---- Override completion keymaps
+
+vim.keymap.set("i", "<Tab>", function()
+  if require("blink.cmp").is_visible() then
+    require("blink.cmp").accept()
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+  end
+end, { desc = "Accept completion or insert tab" })
