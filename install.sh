1ac0036787b27cea87da64f7dd8912ca75c38e98
Maxime <maxime.averty.pro@gmail.com>
2026-03-10 11:55:50 +0100

    update install script

--- install.sh
+++ install.sh
@@ -6,11 +6,18 @@ cd "$(dirname "$0")"
 echo "==> Installing packages..."
 sudo pacman -S --needed - <packages.txt
 
+echo "==> Installing AUR packages..."
+yay -S --needed - <packages-aur.txt
+
 echo "==> Copying configs..."
 for dir in config/*/; do
   name=$(basename "$dir")
   echo "  -> $name"
   cp -r "$dir" ~/.config/
 done
+
+echo "==> Setting permissions..."
+chmod +x ~/.config/rofi/launchers/**/*.sh
+chmod +x ~/.config/waybar/scripts/*.sh
 
 echo "==> Done! Reboot or reload"
